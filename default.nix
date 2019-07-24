731c375f85b14873e5cd7554d763555870213a43
Keith Collister <hnefatl@gmail.com>
2019-07-24 12:59:34 +0100

    Initial commit, packaged the i3blocks scripts into a package with hopefully all required dependencies

---
+++ default.nix
@@ -0,0 +1,15 @@
+let
+    pkgs = import <nixpkgs> { };
+in
+    pkgs.stdenv.mkDerivation {
+        name = "i3blocks-script";
+
+        phases = [ "installPhase" "fixupPhase" ];
+
+        buildInputs = with pkgs; [ bash perl acpi libnotify blueman bluez bluez-tools sysstat i3 networkmanager iproute playerctl pulseaudio ];
+
+        installPhase = ''
+            mkdir -p "$out/bin"
+            cp "${./scripts}/"* "$out/bin/"
+        '';
+    }
