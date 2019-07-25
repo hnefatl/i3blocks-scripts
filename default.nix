e10833ed110d49e554445ed19df6f9429fa5c7f9
Keith Collister <hnefatl@gmail.com>
2019-07-25 11:51:42 +0100

    Moved bluetooth-connected out of this script bundle

--- default.nix
+++ default.nix
@@ -8,6 +8,12 @@ in
 
         inherit (pkgs) bash perl acpi libnotify blueman bluez sysstat i3 networkmanager iproute playerctl pulseaudio kmod alsaUtils procps brightnessctl;
         bluez_tools = pkgs.bluez-tools; # Can't inherit as `-` isn't allowed in env var names.
+        useful_scripts = import (pkgs.fetchFromGitHub {
+            owner = "hnefatl";
+            repo = "useful-scripts";
+            rev = "686872ff523ea1c719fcf935a66f394b058bffd5";
+            sha256 = "137zn45iv73iq033xvclpkpvj2c89pp8hbkxj66xmf1xah030m6p";
+        });
 
         installPhase = ''
             mkdir -p "$out/bin"
