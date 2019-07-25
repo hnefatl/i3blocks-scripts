6f3a219653c3ef811fc8b6de31c1c2b80439a281
Keith Collister <hnefatl@gmail.com>
2019-07-25 11:31:31 +0100

    Added brightness script, improved script installation method

--- default.nix
+++ default.nix
@@ -6,22 +6,15 @@ in
 
         phases = [ "installPhase" "fixupPhase" ];
 
-        inherit (pkgs) bash perl acpi libnotify blueman bluez sysstat i3 networkmanager iproute playerctl pulseaudio kmod alsaUtils procps;
+        inherit (pkgs) bash perl acpi libnotify blueman bluez sysstat i3 networkmanager iproute playerctl pulseaudio kmod alsaUtils procps brightnessctl;
         bluez_tools = pkgs.bluez-tools; # Can't inherit as `-` isn't allowed in env var names.
 
         installPhase = ''
             mkdir -p "$out/bin"
-            substituteAll "${./scripts/battery}" "$out/bin/battery"
-            substituteAll "${./scripts/bluetooth}" "$out/bin/bluetooth"
-            substituteAll "${./scripts/bluetooth-connected}" "$out/bin/bluetooth-connected"
-            substituteAll "${./scripts/cpu_usage}" "$out/bin/cpu_usage"
-            substituteAll "${./scripts/diary}" "$out/bin/diary"
-            substituteAll "${./scripts/disk}" "$out/bin/disk"
-            substituteAll "${./scripts/iface}" "$out/bin/iface"
-            substituteAll "${./scripts/memory}" "$out/bin/memory"
-            substituteAll "${./scripts/spotify}" "$out/bin/spotify"
-            substituteAll "${./scripts/volume}" "$out/bin/volume"
-            substituteAll "${./scripts/wifi}" "$out/bin/wifi"
+            for f in "${./scripts}"/* ; do
+                f="$(basename $f)"
+                substituteAll "${./scripts}/$f" "$out/bin/$f"
+            done
 
             chmod +x "$out/bin/"*
         '';
